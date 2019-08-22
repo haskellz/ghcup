a4b7405a448e9a3efeadffda15a0b0d26517f06a
Woodson Delhia <woodsondelhia88@gmail.com>
2019-08-22 05:52:30 -0400

    Bootstrap somewhat completed need to get ghcup.ps1 working, ghcup regular flags options and view complete. The heavy lifting will start from now

--- boostrap-haskell-windows.ps1
+++ boostrap-haskell-windows.ps1
@@ -1,18 +1,48 @@
 New-Variable -Force -Name GHCUP_INSTALL_BASE_PREFIX -Value "$HOME"
 
-Function Kill-Process($message) {
+Function Kill-Process($message) 
+{
     Write-Host "$message failed!" -ForegroundColor Red 
     Exit 2
 }
 
-Function Try-Command($command){
-    $exit_code = Invoke-Command -ScriptBlock { 
-        Invoke-Expression -command $command
-        $LASTEXITCODE
-    }
+Function Try-Command($command)
+{
+    $cmd = Invoke-Expression -Command "$command -ErrorAction SilentlyContinue"
 
-    if ($exit_code -gt 0) {
+    if (-not $cmd) 
+    {
         Kill-Process $command
+    }
+}
+
+Function Wait-User-Response ($message)
+{
+    # Check if running Powershell ISE
+    if ($psISE)
+    {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show("$message")
+    }
+    else
+    {
+        Write-Host "$message" -ForegroundColor Yellow
+        $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    }
+}
+
+Function Check-Ghcup-Exist
+{
+    $expr = "Get-Command -CommandType Application ghci2" 
+    $cmd = Invoke-Expression -Command "$expr -ErrorAction SilentlyContinue"
+
+    if ($cmd) 
+    {
+        Try-Command "ghcup upgrade"
+    }
+    else 
+    {
+        echo "false"
     }
 }
 
@@ -35,10 +65,43 @@ PATH components.
 
 "@
 
-$ghcup? = Get-Command -CommandType Application ghci -ErrorAction SilentlyContinue
+Write-Host "To proceed with the ghcup installation press enter, to cancel press ctrl-c."
+Write-Host "Note that this script can be re-run at any given time."
+# Wait for user input to continue.
+# shellcheck disable=SC2034
+read-host “Press ENTER to continue...”
 
-if ($ghcup?) {
-    Try-Command "ghcup upgrade"
-}else {
-    echo "false"
+
+if (Get-Command -CommandType Application ghcup -ErrorAction SilentlyContinue) 
+{
+    echo "exist"
 }
+else 
+{
+    try 
+    {
+        Invoke-WebRequest -Uri 'https://github.com/haskellz/ghcup/raw/windows-support/ghcup.ps1' -OutFile $GHCUP_INSTALL_BASE_PREFIX\.ghcup\bin\ghcup.ps1
+        Write-Host -ForegroundColor Yellow "Adding $HOME\.cabal\bin and $GHCUP_INSTALL_BASE_PREFIX\.ghcup\bin to PATH environment variable..."
+        Add-Content -Path $Profile.CurrentUserAllHosts -Value '$Env:Path += ";$HOME\.cabal\bin;$GHCUP_INSTALL_BASE_PREFIX\.ghcup\bin"'
+        refreshenv
+    }
+    catch
+    {
+        Kill-Process "Invoke-WebRequest"
+    }
+    
+}
+
+# Try-Command "ghcup --cache install"
+# Try-Command "ghcup set"
+# Try-Command "ghcup --cache install-cabal"
+# Try-Command "cabal new-update"
+
+Write-Host -ForegroundColor Green @"
+
+Installation done!
+
+"@ 
+
+
+
--- ghcup.ps1
+++ ghcup.ps1
@@ -22,7 +22,206 @@
 # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 # INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-Write-Error -Exception NotImplemeted 
+
+
+
+# @VARIABLE: VERSION
+# @DESCRIPTION:
+# Version of this script.
+$VERSION = "0.0.1"
+
+# @VARIABLE: VERBOSE
+# @DESCRIPTION:
+# Whether to print verbose messages in this script.
+$VERBOSE = $false 
+
+# @VARIABLE: FORCE
+# @DESCRIPTION:
+# Whether to force installation and overwrite files.
+$FORCE = false
+
+# @VARIABLE: GHCUP_INSTALL_BASE_PREFIX
+# @DESCRIPTION:
+# The main install directory prefix, under which .ghcup
+# directory will be created. This directory is user
+# configurable via the environment variable of the
+# same name. It must be non-empty and the path
+# it points to must exist.
+$GHCUP_INSTALL_BASE_PREFIX = $HOME
+
+# @VARIABLE: INSTALL_BASE
+# @DESCRIPTION:
+# The main install directory where all ghcup stuff happens.
+$INSTALL_BASE = "$GHCUP_INSTALL_BASE_PREFIX/.ghcup"
+
+# @VARIABLE: GHC_LOCATION
+# @DESCRIPTION:
+# The location where ghcup will install different ghc versions.
+# This is expected to be a subdirectory of INSTALL_BASE.
+$GHC_LOCATION = "$INSTALL_BASE/ghc"
+
+# @VARIABLE: BIN_LOCATION
+# @DESCRIPTION:
+# The location where ghcup will create symlinks for GHC binaries.
+# This is expected to be a subdirectory of INSTALL_BASE.
+$BIN_LOCATION = "$INSTALL_BASE/bin"
+
+# @VARIABLE: CACHE_LOCATION
+# @DESCRIPTION:
+# The location where ghcup will put tarballs for caching.
+# This is expected to be a subdirectory of INSTALL_BASE.
+$CACHE_LOCATION = "$INSTALL_BASE/cache"
+
+# @VARIABLE: GHC_DOWNLOAD_BASEURL
+# @DESCRIPTION:
+# Base URL for all GHC tarballs.
+$GHC_DOWNLOAD_BASEURL = "https://downloads.haskell.org/~ghc"
+
+# @VARIABLE: SOURCE
+# @DESCRIPTION:
+# the script name.
+$SOURCE = $MyInvocation.ScriptName
+
+# @VARIABLE: BASE_DOWNLOAD_URL
+# DESCRIPTION:
+# The base url for downloading stuff like meta files, requirements files etc.
+$BASE_DOWNLOAD_URL = "https://github.com/haskellz/ghcup/raw/master/"
+
+# @VARIABLE: SCRIPT_UPDATE_URL
+# @DESCRIPTION:
+# Location to update this script from.
+$SCRIPT_UPDATE_URL = "${BASE_DOWNLOAD_URL}/ghcup"
+
+# @VARIABLE: GHCUP_META_DOWNLOAD_URL & META_DOWNLOAD_URL 
+# DESCRIPTION:
+# The url of the meta file for getting
+# download information for ghc/cabal-install etc.
+$META_DOWNLOAD_URL = "${BASE_DOWNLOAD_URL}/.download-urls"
+
+# @VARIABLE: META_DOWNLOAD_FORMAT
+# DESCRIPTION:
+# The version of the meta file format.
+# This determines whether this script can read the
+# file from "${META_DOWNLOAD_URL}".
+$META_DOWNLOAD_FORMAT = "1"
+
+# @VARIABLE: META_VERSION_URL
+# DESCRIPTION:
+# The url of the meta file for getting
+# available versions for ghc/cabal-install etc.
+$META_VERSION_URL = "${BASE_DOWNLOAD_URL}/.available-versions}"
+
+# @VARIABLE: META_VERSION_FORMAT
+# DESCRIPTION:
+# The version of the meta file format.
+# This determines whether this script can read the
+# file from "${META_VERSION_URL}".
+$META_VERSION_FORMAT = "1"
+
+# @VARIABLE: BUG_URL
+# DESCRIPTION:
+# The url to report bugs to.
+$BUG_URL = "https://gitlab.haskell.org/haskell/ghcup/issues"
+
+# @VARIABLE: CACHING
+# @DESCRIPTION:
+# Whether to cache tarballs in $CACHE_LOCATION.
+$CACHING = $false
+
+
+Function Write-Usage 
+{
+    $compile = if ($VERBOSE) { "`n    compile            Compile and install GHC from source (UNSTABLE!!!)" } else { "" }
+    Write-Host @"
+ghcup $VERSION
+GHC up toolchain installer
+
+USAGE:
+    ${SCRIPT} [FLAGS] <SUBCOMMAND>
+
+FLAGS:
+    -v, --verbose    Enable verbose output
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+    -w, --wget       Use wget instead of curl
+    -c, --cache      Use \"${CACHE_LOCATION}\" for caching tarballs
+                        (these will not be removed by ghcup)
+
+SUBCOMMANDS:
+    install            Install GHC $compile
+    set                Set currently active GHC version
+    list               Show available GHCs and other tools
+    upgrade            Upgrade this script in-place
+    rm                 Remove an already installed GHC
+    install-cabal      Install cabal-install
+    debug-info         Print debug info (e.g. detected system/distro)
+    changelog          Show the changelog of a GHC release (online)
+    print-system-reqs  Print an approximation of system requirements
+
+DISCUSSION:
+    ghcup installs the Glasgow Haskell Compiler from the official
+    release channels, enabling you to easily switch between different
+    versions.
+"@
+    Exit 1
+}
+
+if ($args.Length -eq 0)
+{
+    Write-Usage
+    Exit 1
+}
+
+Function Write-List-Commands
+{
+    Write-Host @"
+changelog
+compile
+debug-info
+install
+install-cabal
+list
+print-system-reqs
+rm
+set
+upgrade
+"@
+    Exit 0
+}
+
+Function Shift-Args-Left 
+{
+    $head,$rest = $script:args 
+    $script:args = $rest 
+}
+
+while ($args.Length -gt 0)
+{
+    switch ($args.get(0))
+    {
+        { $_ -cin @("-h", "--help")    } { Write-Usage }
+        { $_ -cin @("-V", "--version") } { Write-Host "$VERSION"; Exit 0 }
+        { $_ -ceq "--list-commands"    } { Write-List-Commands }
+        { $_ -cin @("-v", "--verbose") } 
+        { 
+            $VERBOSE = $true; 
+            Shift-Args-Left
+            if ($args.Length -lt 1) { Write-Usage } 
+        }
+        { $_ -cin @("-c", "--cache") } 
+        { 
+            $CACHING = $true; 
+            Shift-Args-Left
+            if ($args.Length -lt 1) { Write-Usage } 
+        }
+        default ## startup tasks ##
+        {
+
+        }
+        
+    }
+}
